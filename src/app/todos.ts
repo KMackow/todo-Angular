addc9dc38cc69e2398bc60a8350eaaf2342dfe43
KMackow <kamilmackow@hotmail.co.uk>
2018-09-14 16:06:06 +0100

    Revert "Revert "minor changes to formating so actions dont overlap on dashboard""
    This reverts commit c7df08987af8c699593be1369e28cc5564e51f90.

--- src/app/todos.ts
+++
@@ -1,9 +0,0 @@
-import {Todo} from "./todo";
-
-export const TODOS: Todo[] = [
-    {id: 1,  title: "shopping", isComplete: false},
-    {id: 2,  title: "take rubish out", isComplete: true},
-    {id: 3,  title: "washing up", isComplete: false},
-    {id: 4,  title: "prepare lunch", isComplete: true},
-    {id: 5,  title: "shopping", isComplete: false},
-]
