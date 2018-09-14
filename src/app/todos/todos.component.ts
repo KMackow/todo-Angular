83987445fc8df9052cd51cfd228ba18a6dcd2c6e
KMackow <kamilmackow@hotmail.co.uk>
2018-09-14 16:04:31 +0100

    Revert "fix"
    This reverts commit 1cfa0578f34e62bb2d71f47c452fa4abeab8ef72.

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -14,6 +14,9 @@ export class TodosComponent implements OnInit {
   filter: string = "All";  
   constructor(private todoService: TodoService) { }
 
+  onMove() {
+  
+}
 
   changeFilter(filter: string): void {
     this.filter = filter;
